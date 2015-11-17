5c9f9e6a4a143cf275f3da2956c9b55185847ccd
Chris Campbell <chriscampbell@TESTs-MacBook-Air.local>
2015-11-17 12:23:58 -0500

    javascript click game with 20 seconf timer

---
+++ clickgame.js
@@ -0,0 +1,10 @@
+var counter = 0;
+function countup() {
+    counter = counter + 1;
+}
+
+function countdown() {
+    setTimeout(function countandgive() {
+        alert("Time is up, you clicked " +counter+ " portraits!");
+    }, 20000);
+}
