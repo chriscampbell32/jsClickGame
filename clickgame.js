8b39ff358a8df2319ece275c9fa1ad5b6584793e
steph-harris <steph.harris21@gmail.com>
2016-01-06 01:42:24 -0500

    fixed indents in js file

--- clickgame.js
+++ clickgame.js
@@ -1,10 +1,10 @@
 var counter = 0;
-function countup() {
-    counter = counter + 1;
-}
+function countup(){
+  counter = counter + 1;
+};
 
-function countdown() {
-    setTimeout(function countandgive() {
-        alert("Time is up, you clicked " +counter+ " portraits!");
-    }, 20000);
-}
+function countdown(){
+  setTimeout(function countandgive(){
+    alert("Time is up, you clicked " +counter+ " portraits!");
+  }, 20000);
+};
