285d5417ae874fd5a563521d6735376b4f09f3ca
steph-harris <steph.harris21@gmail.com>
2016-01-11 15:01:06 -0500

    added pseudocode, jquery, and commented out doc ready code

--- clickgame.js
+++ clickgame.js
@@ -1,10 +1,17 @@
-var counter = 0;
-function countup(){
-  counter++;
-};
+//$(document).ready(function(){
+  var counter = 0;
+  function countup(){
+    counter++;
+  };
 
-function countdown(){
-  setTimeout(function countandgive(){
-    alert("Time is up, you clicked " +counter+ " portraits!");
-  }, 20000);
-};
+  function countdown(){
+    setTimeout(function countandgive(){
+      alert("Time is up, you clicked " +counter+ " portraits!");
+    }, 20000);
+  };
+//});
+//add jQuery
+//add doc ready
+//add click event to button that starts countdown
+//add click event to img-responsives that runs countup function
+// += and disables image from being clicked again
