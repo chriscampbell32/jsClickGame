4c110c2b58085499a64ae43483a045d5b9525292
steph-harris <steph.harris21@gmail.com>
2016-01-11 22:50:39 -0500

    started work on showing game score

--- clickgame.js
+++ clickgame.js
@@ -22,6 +22,7 @@ $(document).ready(function(){
 
   function pointCountup(){
     scoreCounter++;
+    $(".container").append("<h2>Score:" + scoreCounter + "</h2>")
   };
 
   function gameCountdown(){
